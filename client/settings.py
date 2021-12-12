2715ab87ccaf18c0d1820cba24ae03c1f6022c1b
kim-chae-yeon <58394861+kim-chae-yeon@users.noreply.github.com>
2021-12-12 22:38:50 +0900

    programming error 수정 중

--- client/settings.py
+++ client/settings.py
@@ -83,10 +83,6 @@ WSGI_APPLICATION = 'client.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
         'ENGINE': 'djongo',
         'NAME': 'lecture',
